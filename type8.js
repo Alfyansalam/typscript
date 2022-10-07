7c129f89e96293f1600e7bf58c3e4d3d18c1c8cf
Alfyansalam <alfyaleena@gmail.com>
2022-10-07 17:59:22 +0530

    ok

---
+++ type8.js
@@ -0,0 +1,28 @@
+var grade = "A";
+switch (grade) {
+    case "A":
+        {
+            console.log("Excellent");
+            break;
+        }
+    case "B":
+        {
+            console.log("good");
+            break;
+        }
+    case "C":
+        {
+            console.log("fair");
+            break;
+        }
+    case "D":
+        {
+            console.log("poor");
+            break;
+        }
+    default:
+        {
+            console.log("invalid choice");
+            break;
+        }
+}
